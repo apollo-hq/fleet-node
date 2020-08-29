8256f02c0e45ab359adcf5b3aabcaa008abdf40b
Karolis Rusenas <karolis.rusenas@gmail.com>
2020-08-29 15:18:48 +0100

    install buf

---
+++ install_buf.sh
@@ -0,0 +1,7 @@
+# Substitute PREFIX for your install prefix.
+# Substitute VERSION for the current released version.
+PREFIX="/usr/local" && \
+VERSION="0.20.5" && \
+  curl -sSL \
+    "https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf-$(uname -s)-$(uname -m).tar.gz" | \
+    tar -xvzf - -C "${PREFIX}" --strip-components 1

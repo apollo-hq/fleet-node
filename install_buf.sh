b7de37c2b1339d3928c6ad988bbbb81988d477a2
Karolis Rusenas <karolis@rusenas.dev>
2021-01-24 18:17:23 +0000

    update proto

--- install_buf.sh
+++ install_buf.sh
@@ -1,7 +1,7 @@
 # Substitute PREFIX for your install prefix.
 # Substitute VERSION for the current released version.
 PREFIX="/usr/local" && \
-VERSION="0.20.5" && \
+VERSION="0.28.0" && \
   curl -sSL \
     "https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf-$(uname -s)-$(uname -m).tar.gz" | \
     tar -xvzf - -C "${PREFIX}" --strip-components 1
